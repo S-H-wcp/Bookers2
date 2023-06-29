838d1014adc39096caa53eab2f3f62cc8820e2c0
S-H-wcp <134398710+S-H-wcp@users.noreply.github.com>
2023-06-29 15:16:32 +0000

    [Finish] Task4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_29_130831) do
+ActiveRecord::Schema.define(version: 2023_06_29_140318) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,13 @@ ActiveRecord::Schema.define(version: 2023_06_29_130831) do
     t.string "title"
     t.string "body"
     t.integer "user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
